8338bf7f9566023c5514f83a716278a068a683cb
Dave Holst <daveholst@gmail.com>
2021-04-06 17:58:32 +0800

    tute progress

--- index.js
+++ index.js
@@ -6,12 +6,47 @@ const fetchData = async (searchTerm) => {
     }
   });
 
-  console.log(response.data)
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search for a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results">
+    </div>
+  </div>
+`;
+
 const input = document.querySelector('input');
-input.addEventListener('input', (event) => {
-  fetchData(event.target.value);
-})
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+
+
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+  //make dropdown visble
+  dropdown.classList.add('is-active');
+
+  for (let movie of movies) {
+    const listOption = document.createElement('a');
+    listOption.classList.add('dropdown-item');
+    listOption.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+
+    resultsWrapper.appendChild(listOption);
+  }
+};
+
+input.addEventListener('input', debounce(onInput, 500))
 
 
