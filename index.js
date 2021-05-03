22140f7a54b39eeb6800c71ee9e6ef6299816475
Dave Holst <daveholst@gmail.com>
2021-05-03 13:36:41 +0800

    2 search fields printing to each side

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-
-createAutoComplete({
-  root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     return`
@@ -9,9 +6,7 @@ createAutoComplete({
       <h1>${movie.Title} (${movie.Year})</h1>
     `;
   },
-  onOptionSelect(movie) {
-    onMovieSelect(movie);
-  },
+
   inputValue(movie) {
     return movie.Title;
   },
@@ -29,18 +24,36 @@ createAutoComplete({
 
     return response.data.Search;
   }
+}
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden')
+    onMovieSelect(movie, document.querySelector('#left-summary'));
+  },
+});
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden')
+    onMovieSelect(movie, document.querySelector('#right-summary'));
+  },
 });
 
 
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '4be7587f',
       i: movie.imdbID
     }
   });
-  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 }
 
 const movieTemplate = (movieDetail) => {
